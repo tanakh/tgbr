45c14b46254cca60e7af9172a83577b76e126d0c
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2022-02-08 09:11:01 +0900

    Fix DMA timing behavior

--- src/bus.rs
+++ src/bus.rs
@@ -19,9 +19,8 @@ pub struct Bus {
 struct Dma {
     source: u8,
     pos: u8,
-    buf: u8,
     enabled: bool,
-    phase: bool,
+    delay: usize,
 }
 
 impl Bus {
@@ -50,7 +49,20 @@ impl Bus {
     }
 
     pub fn read(&mut self, addr: u16) -> u8 {
-        let data = match addr {
+        let data = self.read_(addr);
+        trace!("<-- Read:  ${addr:04X} = ${data:02X}");
+        data
+    }
+
+    pub fn read_immutable(&mut self, addr: u16) -> Option<u8> {
+        match addr {
+            0xff00..=0xff7f | 0xffff => None,
+            _ => Some(self.read_(addr)),
+        }
+    }
+
+    fn read_(&mut self, addr: u16) -> u8 {
+        match addr {
             0x0000..=0x00FF => {
                 if self.map_boot_rom {
                     self.boot_rom[addr as usize]
@@ -63,10 +75,10 @@ impl Bus {
             0xa000..=0xbfff => self.mbc.borrow_mut().read(addr),
             0xc000..=0xfdff => self.ram[(addr & 0x1fff) as usize],
             0xfe00..=0xfe9f => {
-                if !*self.oam_lock.borrow() {
+                if !*self.oam_lock.borrow() && !self.dma.enabled {
                     self.oam.borrow_mut()[(addr & 0xff) as usize]
                 } else {
-                    0xff
+                    !0
                 }
             }
             0xfea0..=0xfeff => todo!("Read from Unusable address: ${addr:04x}"),
@@ -80,22 +92,24 @@ impl Bus {
             }
             0xff80..=0xfffe => self.hiram[(addr & 0x7f) as usize],
             0xffff => self.io.borrow_mut().read(addr),
-        };
-        trace!("Read: ${addr:04X} = ${data:02X}");
-        data
+        }
     }
 
     pub fn write(&mut self, addr: u16, data: u8) {
-        // FIXME: durgin DMA, CPU can access only HiRAM
-
-        trace!("Write: ${addr:04X} = ${data:02X}");
+        trace!("--> Write: ${addr:04X} = ${data:02X}");
+        if self.dma.enabled {
+            if matches!(addr, 0xff80..=0xfffe) {
+                self.hiram[(addr & 0x7f) as usize] = data;
+            }
+            return;
+        }
         match addr {
             0x0000..=0x7fff => self.mbc.borrow_mut().write(addr, data),
             0x8000..=0x9fff => self.vram.borrow_mut()[(addr & 0x1fff) as usize] = data,
             0xa000..=0xbfff => self.mbc.borrow_mut().write(addr, data),
             0xc000..=0xfdff => self.ram[(addr & 0x1fff) as usize] = data,
             0xfe00..=0xfe9f => {
-                if !*self.oam_lock.borrow() {
+                if !*self.oam_lock.borrow() && !self.dma.enabled {
                     self.oam.borrow_mut()[(addr & 0xff) as usize] = data
                 }
             }
@@ -105,7 +119,8 @@ impl Bus {
                 // DMA
                 self.dma.source = data;
                 self.dma.pos = 0;
-                self.dma.enabled = true;
+                self.dma.enabled = false;
+                self.dma.delay = 2;
             }
             0xff50 => self.map_boot_rom = data == 0, // BANK
             0xff00..=0xff7f => self.io.borrow_mut().write(addr, data),
@@ -114,28 +129,33 @@ impl Bus {
         };
     }
 
-    pub fn read_immutable(&mut self, addr: u16) -> Option<u8> {
-        match addr {
-            0xff00..=0xff7f | 0xffff => None,
-            _ => Some(self.read(addr)),
-        }
+    pub fn tick(&mut self) {
+        self.process_dma();
+        self.io.borrow_mut().tick();
     }
 
-    pub fn tick(&mut self) {
-        if self.dma.enabled {
-            if !self.dma.phase {
-                self.dma.buf = self.read((self.dma.source as u16) << 8 | self.dma.pos as u16);
-                self.dma.phase = true;
-            } else {
-                self.write(0xFE00 | self.dma.pos as u16, self.dma.buf);
-                self.dma.phase = false;
-                self.dma.pos += 1;
-                if self.dma.pos == 0xA0 {
-                    self.dma.enabled = false;
-                }
+    fn process_dma(&mut self) {
+        if self.dma.delay > 0 {
+            self.dma.delay -= 1;
+            if self.dma.delay == 0 {
+                self.dma.enabled = true;
             }
+            return;
         }
-
-        self.io.borrow_mut().tick();
+        if !self.dma.enabled {
+            return;
+        }
+        log::trace!(
+            "<-> DMA:   ${:02X}{:02X} -> $FE{:02X}",
+            self.dma.source,
+            self.dma.pos,
+            self.dma.pos
+        );
+        let data = self.read_((self.dma.source as u16) << 8 | self.dma.pos as u16);
+        self.oam.borrow_mut()[self.dma.pos as usize] = data;
+        self.dma.pos += 1;
+        if self.dma.pos == 0xA0 {
+            self.dma.enabled = false;
+        }
     }
 }
